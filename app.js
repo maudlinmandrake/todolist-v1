8cb173285d0b3545e386a1633de316b7853f02fa
Jennifer Mikac <jennymikac@gmail.com>
2020-07-17 10:56:25 -0400

    Change if/else to switch

--- app.js
+++ app.js
@@ -14,20 +14,31 @@ app.get("/", function(req, res){
     var day = "";
 
     //what day of the week is it?
-    if (currentDay === 0) {
-        day = "Sunday"
-    } else if (currentDay === 1) {
-        day = "Monday"
-    } else if (currentDay === 2) {
-        day = "Tuesday"
-    } else if (currentDay === 3) {
-        day = "Wednesday"
-    } else if (currentDay === 4) {
-        day = "Thursday"
-    } else if (currentDay === 5) {
-        day = "Friday"
-    } else if (currentDay === 6) {
-        day = "Saturday"
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+            case 1:
+                day = "Monday"
+                break;
+                case 2:
+                    day = "Tuesday"
+                    break;
+                    case 3:
+                        day = "Wednesday"
+                        break;
+                        case 4:
+                            day = "Thursday"
+                            break;
+                            case 5:
+                                day = "Friday"
+                                break;
+                                case 6:
+                                    day = "Saturday"
+                                    break;
+    
+        default:
+            break;
     }
 
     //sned day of week to list.ejs
