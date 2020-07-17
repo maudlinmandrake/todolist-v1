e53f9a6a1f187b1b8bce9d4c73d863da8b3295f4
Jennifer Mikac <jennymikac@gmail.com>
2020-07-17 10:59:16 -0400

    Log error on switch statement

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
     var today = new Date();
     var currentDay = today.getDay();
@@ -18,34 +18,36 @@ app.get("/", function(req, res){
         case 0:
             day = "Sunday"
             break;
-            case 1:
-                day = "Monday"
-                break;
-                case 2:
-                    day = "Tuesday"
-                    break;
-                    case 3:
-                        day = "Wednesday"
-                        break;
-                        case 4:
-                            day = "Thursday"
-                            break;
-                            case 5:
-                                day = "Friday"
-                                break;
-                                case 6:
-                                    day = "Saturday"
-                                    break;
-    
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
         default:
+        console.log("The current day is" + currentDay + "...are you on a different time-space continuum?");
             break;
     }
 
     //sned day of week to list.ejs
-    res.render("list", {kindOfDay: day});
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
