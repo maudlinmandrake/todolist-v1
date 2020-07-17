4e97ed1feaf2c433e3ccd274154ed7a2f5333dea
Jennifer Mikac <jennymikac@gmail.com>
2020-07-17 11:47:46 -0400

    A better way to determine and grab date

--- app.js
+++ app.js
@@ -10,36 +10,13 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    //what day of the week is it?
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-        console.log("The current day is" + currentDay + "...are you on a different time-space continuum?");
-            break;
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
     //sned day of week to list.ejs
     res.render("list", {
