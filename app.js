afc532bdd3efc81106d01df500bcb34e1173a930
Jennifer Mikac <jennymikac@gmail.com>
2020-07-19 13:26:52 -0400

    Update app to get about page

--- app.js
+++ app.js
@@ -52,12 +52,16 @@ app.post("/", function(req, res){
 });
 
 //post new item to list and redirect to work route
-app.post("/work", function(){
+app.get("/work", function(){
     let item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 });
 
-app.listen(3000, function () {
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
